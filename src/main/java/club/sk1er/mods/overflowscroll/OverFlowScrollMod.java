ceffce26b320907b0cf7df18190f2f9c061ec894
asbyth <cyronize@gmail.com>
2019-10-24 08:54:53 -0400

    unused event 1.9

--- src/main/java/club/sk1er/mods/overflowscroll/OverFlowScrollMod.java
+++ src/main/java/club/sk1er/mods/overflowscroll/OverFlowScrollMod.java
@@ -1,7 +1,6 @@
 package club.sk1er.mods.overflowscroll;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = OverFlowScrollMod.MOD_ID, name = OverFlowScrollMod.MOD_NAME, version = OverFlowScrollMod.MOD_VERSION, clientSideOnly = true)
 public class OverFlowScrollMod {
@@ -9,10 +8,4 @@ public class OverFlowScrollMod {
     public static final String MOD_ID = "text_overflow_scroll";
     public static final String MOD_VERSION = "1.0";
     public static final String MOD_NAME = "Text Overflow Scroll";
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-    }
-
 }
