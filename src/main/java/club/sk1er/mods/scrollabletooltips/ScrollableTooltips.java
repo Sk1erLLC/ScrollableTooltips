a690865643d6cd5b5015d187b9aee6f60d83b3c4
asbyth <cyronize@gmail.com>
2021-05-22 17:21:24 -0400

    Build: Update to latest ModCore

--- src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
@@ -8,14 +8,15 @@ import net.modcore.api.ModCoreAPI;
 @Mod(modid = ScrollableTooltips.MOD_ID, name = ScrollableTooltips.MOD_NAME, version = ScrollableTooltips.MOD_VERSION, clientSideOnly = true)
 public class ScrollableTooltips {
 
+    // todo: migrate modid to scrollable_tooltips
     public static final String MOD_ID = "text_overflow_scroll";
-    public static final String MOD_VERSION = "1.3";
-    public static final String MOD_NAME = "Text Overflow Scroll";
-
+    public static final String MOD_VERSION = "1.4";
+    public static final String MOD_NAME = "Scrollable Tooltips";
 
     @Mod.EventHandler
     public void loadComplete(FMLLoadCompleteEvent event) {
-        if (!ForgeVersion.getVersion().contains("2318") && ForgeVersion.mcVersion.equalsIgnoreCase("1.8.9")) { //Not always true due to other vers
+        //noinspection ConstantConditions
+        if (!ForgeVersion.getVersion().contains("2318") && ForgeVersion.mcVersion.equalsIgnoreCase("1.8.9")) {
             ModCoreAPI.getNotifications().push(
                     "Outdated Forge",
                     "Scrollable Tooltips will not work on anything below Forge build 2318.\nPlease consider updating Forge."
--- src/main/java/club/sk1er/mods/scrollabletooltips/tweaker/ScrollableTooltipsTweaker.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/tweaker/ScrollableTooltipsTweaker.java
@@ -1,11 +1,10 @@
 package club.sk1er.mods.scrollabletooltips.tweaker;
 
-
 import club.sk1er.mods.scrollabletooltips.forge.FMLLoadingPlugin;
 import net.modcore.loader.ModCoreSetupTweaker;
 
+@SuppressWarnings("unused")
 public class ScrollableTooltipsTweaker extends ModCoreSetupTweaker {
-
     public ScrollableTooltipsTweaker() {
         super(new String[]{FMLLoadingPlugin.class.getName()});
     }
