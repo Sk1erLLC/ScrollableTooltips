20656863afc5b2109b74b86539b78f5885b515ca
asbyth <cyronize@gmail.com>
2021-05-25 07:31:03 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/mods/scrollabletooltips/tweaker/ScrollableTooltipsTweaker.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/tweaker/ScrollableTooltipsTweaker.java
@@ -1,10 +1,10 @@
 package club.sk1er.mods.scrollabletooltips.tweaker;
 
 import club.sk1er.mods.scrollabletooltips.forge.FMLLoadingPlugin;
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class ScrollableTooltipsTweaker extends EssentialTweaker {
+public class ScrollableTooltipsTweaker extends EssentialSetupTweaker {
     public ScrollableTooltipsTweaker() {
         super(new String[]{FMLLoadingPlugin.class.getName()});
     }
