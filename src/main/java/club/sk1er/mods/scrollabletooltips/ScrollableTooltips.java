71a83b28ba3aa1ef69f193458acba916c90cd8f5
Sychic <47618543+Sychic@users.noreply.github.com>
2022-11-16 10:51:50 -0500

    Change: Migrate Mod ID and add warning

--- src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
@@ -5,6 +5,7 @@ import gg.essential.api.EssentialAPI;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
@@ -13,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 public class ScrollableTooltips {
 
     // todo: migrate modid to scrollable_tooltips
-    public static final String MOD_ID = "text_overflow_scroll";
+    public static final String MOD_ID = "scrollable_tooltips";
     public static final String MOD_VERSION = "1.4.0";
     public static final String MOD_NAME = "Scrollable Tooltips";
 
@@ -29,6 +30,12 @@ public class ScrollableTooltips {
             EssentialAPI.getNotifications().push(
                     "Outdated Forge",
                     "Scrollable Tooltips will not work on anything below Forge build 2318.\nPlease consider updating Forge."
+            );
+        }
+        if (Loader.isModLoaded("text_overflow_scroll")) {
+            EssentialAPI.getNotifications().push(
+                    "Duplicate Scrollable Tooltips",
+                    "Scrollable tooltips has detected an older version. \nPlease remove the older version to avoid possible incompatibilities."
             );
         }
     }
